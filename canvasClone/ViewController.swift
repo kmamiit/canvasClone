8a4c3692fa85e6d94d77118cdb1ce46e3a33af9d
kmamiit <kmamiit123@gmail.com>
2018-11-22 21:33:55 -0800

    Initial Commit

---
+++ canvasClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  canvasClone
+//
+//  Created by Kyle Mamiit (New) on 11/22/18.
+//  Copyright © 2018 Kyle Mamiit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
